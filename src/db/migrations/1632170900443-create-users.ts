07a06d76f656eba3c65090632f823591bd8d7218
Artur-Barreto <artur.barreto7@outlook.com>
2021-09-20 18:00:29 -0300

    projeto em fase inicial

---
+++ src/db/migrations/1632170900443-create-users.ts
@@ -0,0 +1,42 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createUsers1632170900443 implements MigrationInterface {
+  private table = new Table({
+    name: 'users',
+    columns: [
+      {
+        name: 'id',
+        type: 'integer',
+        isPrimary: true,
+        isGenerated: true,
+        generationStrategy: 'increment',
+      },
+      {
+        name: 'email',
+        type: 'varchar',
+        length: '255',
+        isUnique: true,
+      },
+      {
+        name: 'created_at',
+        type: 'timestamptz',
+        isNullable: false,
+        default: 'now()',
+      },
+      {
+        name: 'updated_at',
+        type: 'timestamptz',
+        isNullable: false,
+        default: 'now()',
+      },
+    ],
+  });
+
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(this.table);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(this.table);
+  }
+}
